b95202c8ddd89fd03fcb0fd76718b2c5ab3c660f
nishant <nishantnks@hotmail.com>
2020-04-18 20:15:46 +0530

    Structuring app for use of Flux/Redux

---
+++ client/src/components/About/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class About extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <>
+                <h2>About Page</h2>
+                <p> This is the about page for devs</p>
+            </>
+        );
+    }
+}
+
+export default About;
---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+    return(
+      <div className="jumbotron">
+          <h1> PS Admin</h1>
+          <p> React, Flux and React Router for PWA</p>
+      </div>
+    );
+}
+
+export default Home;
